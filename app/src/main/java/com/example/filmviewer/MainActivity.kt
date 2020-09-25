e92d5a87efff46a68381727bfe982a50c02c83ec
Maxim190 <vbrc1212@yandex.ru>
2020-09-25 13:42:13 +0300

    fix bug with recyclerView static duplicate background if rotate device

--- app/src/main/java/com/example/filmviewer/MainActivity.kt
+++ app/src/main/java/com/example/filmviewer/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_container, MainViewImpl())
+        transaction.replace(R.id.fragment_container, MainViewImpl())
         transaction.commit()
     }
 }
